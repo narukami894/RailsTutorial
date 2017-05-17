45030aaf0ef97078e4d8843171bb0ae1c7a99bb3
narukami894 <s.yakushiji@icloud.com>
2017-05-17 14:40:52 +0900

    hoge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
-  def full_title
+  def full_title(page_title = '')
+    default_title = 'Ruby on Rails Tutorial Sample App'
+    if page_title.empty?
+      default_title
+    else
+      page_title + ' | ' + default_title
+    end
   end
 end
